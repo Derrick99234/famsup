2b974547adc52413a02f99e5aae7a671cae2a19c
Anyidollar <ifeanyionyekwere247@gmail.com>
2024-07-18 00:23:34 +0100

    moved the Navbar to home page

--- src/pages/Dashboard/NavBar.jsx
+++ src/pages/Dashboard/NavBar.jsx
@@ -11,17 +11,17 @@ const NavBar = () => {
 
   return (
     <>
-      <nav className=" bg-green-600 p-2">
+      <nav className="bg-white  p-2">
         <div className="container mx-auto px-4 py-2 flex justify-between items-center">
           <div className="flex items-center">
             <img src={Frame} alt="Logo" className="h-10 ml-12 cursor-pointer" />
           </div>
           <div className="hidden md:flex space-x-4 items-center">
             <a
-              href="#home"
-              className=" font-[700] text-myblack hover:text-blue-500"
+              href="#ourService"
+              className="text-myblack font-[700] hover:text-blue-500"
             >
-              BLOG
+              ABOUT
             </a>
             <a
               href="#about"
@@ -29,12 +29,7 @@ const NavBar = () => {
             >
               FAQS
             </a>
-            <a
-              href="#ourService"
-              className="text-myblack font-[700] hover:text-blue-500"
-            >
-              API
-            </a>
+
             <a
               href="/signin"
               className="text-myblack font-[700] hover:text-blue-500"
@@ -43,7 +38,7 @@ const NavBar = () => {
             </a>
             <a
               href="/signup"
-              className="bg-blue-600 text-white font-[700] hover:bg-yellow-600 px-4 py-2 rounded-lg"
+              className="bg-green-600 text-white font-[700] hover:bg-indigo-600 px-4 py-2 rounded-lg"
             >
               SIGN UP
             </a>
@@ -61,10 +56,10 @@ const NavBar = () => {
           <div className="md:hidden bg-white border-t border-gray-200">
             <div className="container mx-auto px-4 py-2 space-y-2">
               <a
-                href="#home"
+                href="#ourService"
                 className="block text-gray-700 font-[500] hover:text-blue-500"
               >
-                BLOG
+                ABOUT
               </a>
               <a
                 href="#about"
@@ -72,12 +67,7 @@ const NavBar = () => {
               >
                 FAQS
               </a>
-              <a
-                href="#ourService"
-                className="block text-gray-700 font-[500] hover:text-blue-500"
-              >
-                API
-              </a>
+
               <a
                 href="#contact"
                 className="block text-gray-700 font-[500] hover:text-blue-500"
@@ -86,7 +76,7 @@ const NavBar = () => {
               </a>
               <a
                 href="#"
-                className="block bg-blue-500 font-[500] text-white px-4 py-2 rounded hover:bg-blue-600"
+                className="block bg-green-600 font-[500] text-white px-4 py-2 rounded hover:bg-indigo-600"
               >
                 SIGN UP
               </a>
